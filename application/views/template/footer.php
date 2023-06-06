628c19323a543e2dc1db5839b586d0a5113f0e47
Diego César Fiuza <diego.fiuza2@etec.sp.gov.br>
2023-06-06 16:02:09 -0300

    Subindo versão final

---
+++ application/views/template/footer.php
@@ -0,0 +1,5 @@
+<footer>
+	<div class="container text-center">
+		<p>by <a href="http://diegomariano.com"><span>Diego César De Mello</span></a> | Template by <a href="http://moozthemes.com"><span>MOOZ</span>Themes.com</a></p>
+	</div>
+</footer>
